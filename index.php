b0fff457157e7a068ee9dab3ac73570caa5eace2
mr-blogger-A1 <mohamedmmp@hotmail.com>
2025-08-20 18:41:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,345 @@
+{
+  "YallaKoora-24": [
+    {
+      "Cup-id": 102,
+      "Cup-Name": "كوبا ليبيرتادورس - دور الـ 16",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=102",
+      "Match-id": 4443283,
+      "Team-Right": {
+        "Name": "ساو باولو",
+        "Color": "#FE0000",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1225",
+        "Goal": 1
+      },
+      "Team-Left": {
+        "Name": "اتلتيكو ناسيونال",
+        "Color": "#008C54",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8640",
+        "Goal": 1
+      },
+      "Time-Start": "2025-08-20T02:30",
+      "Time-End": "2025-08-20T04:30",
+      "Time-Now": 125,
+      "Time-Zone": "+02:00",
+      "Match-Status": "بعد ركلات الترجيح",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "Maximiliano Ramírez",
+          "Club-Name": "MorumBIS",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 102,
+      "Cup-Name": "كوبا ليبيرتادورس - دور الـ 16",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=102",
+      "Match-id": 4443293,
+      "Team-Right": {
+        "Name": "راسينغ كلوب",
+        "Color": "#029CDC",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=876",
+        "Goal": 3
+      },
+      "Team-Left": {
+        "Name": "بينارول",
+        "Color": "#FFC900",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=7766",
+        "Goal": 1
+      },
+      "Time-Start": "2025-08-20T02:30",
+      "Time-End": "2025-08-20T04:30",
+      "Time-Now": 90,
+      "Time-Zone": "+02:00",
+      "Match-Status": "انتهت",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "Wilmar Alexander Roldán Pérez",
+          "Club-Name": "ملعب الرئيس خوان دومينغو بيرون",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 552,
+      "Cup-Name": "الدوري المصري",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=552",
+      "Match-id": 4506644,
+      "Team-Right": {
+        "Name": "سيراميكا كليوباترا",
+        "Color": "#E01F1F",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=50882",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "إنبي",
+        "Color": "#330099",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8300",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T17:00",
+      "Time-End": "2025-08-20T19:00",
+      "Time-Now": 37,
+      "Time-Zone": "+02:00",
+      "Match-Status": "الشوط الأول",
+      "info": {
+        "Round": 3,
+        "Match-Info": {
+          "Match-Referee": "Sobhi Wl Amarawy",
+          "Club-Name": "ستاد هيئة قناة السويس",
+          "Tv": "أون سبورت 1"
+        }
+      }
+    },
+    {
+      "Cup-id": 552,
+      "Cup-Name": "الدوري المصري",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=552",
+      "Match-id": 4506645,
+      "Team-Right": {
+        "Name": "وادي دجلة",
+        "Color": "#DED113",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8311",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "بتروجت",
+        "Color": "#162665",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8302",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T20:00",
+      "Time-End": "2025-08-20T22:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": 3,
+        "Match-Info": {
+          "Match-Referee": "Ibrahim Ghuloum Mohamed",
+          "Club-Name": "ستاد السلام",
+          "Tv": "أون سبورت 2"
+        }
+      }
+    },
+    {
+      "Cup-id": 552,
+      "Cup-Name": "الدوري المصري",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=552",
+      "Match-id": 4506646,
+      "Team-Right": {
+        "Name": "الجونة",
+        "Color": "#DED113",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8308",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "غزل المحلة",
+        "Color": "#96E7E9",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=8610",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T20:00",
+      "Time-End": "2025-08-20T22:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": 3,
+        "Match-Info": {
+          "Match-Referee": "وليد عبد الرازق",
+          "Club-Name": "استاد خالد بشارة (الجونة)",
+          "Tv": "أون سبورت 1"
+        }
+      }
+    },
+    {
+      "Cup-id": 332,
+      "Cup-Name": "تصفيات ابطال اوروبا - بلاي اوف",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=332",
+      "Match-id": 4516159,
+      "Team-Right": {
+        "Name": "بودو جليمت",
+        "Color": "#FFCC33",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1978",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "شتورم غراتس",
+        "Color": "#000000",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1741",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T21:00",
+      "Time-End": "2025-08-20T23:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "ساندرو سشارير",
+          "Club-Name": "استاد اسبميرا",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 332,
+      "Cup-Name": "تصفيات ابطال اوروبا - بلاي اوف",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=332",
+      "Match-id": 4522874,
+      "Team-Right": {
+        "Name": "بازل",
+        "Color": "#FF0000",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1138",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "كوبنهاجن",
+        "Color": "#D9D9D9",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1824",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T21:00",
+      "Time-End": "2025-08-20T23:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "أنتوني تايلور",
+          "Club-Name": "سانت ياكوب بارك",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 332,
+      "Cup-Name": "تصفيات ابطال اوروبا - بلاي اوف",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=332",
+      "Match-id": 4522921,
+      "Team-Right": {
+        "Name": "سيلتيك",
+        "Color": "#006333",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=754",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "قايرات ألماته",
+        "Color": "#FFFF00",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=10564",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T21:00",
+      "Time-End": "2025-08-20T23:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "Espen Eskås",
+          "Club-Name": "سيلتك بارك",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 332,
+      "Cup-Name": "تصفيات ابطال اوروبا - بلاي اوف",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=332",
+      "Match-id": 4522923,
+      "Team-Right": {
+        "Name": "فنربخشة",
+        "Color": "#CEC81B",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=946",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "بنفيكا",
+        "Color": "#CC0000",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=888",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-20T21:00",
+      "Time-End": "2025-08-20T23:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "جان سيدل",
+          "Club-Name": "ملعب شكري سراج أوغلو",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 113,
+      "Cup-Name": "الدوري البرازيلي - الدرجة الأولى",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=113",
+      "Match-id": 4380488,
+      "Team-Right": {
+        "Name": "جوفينتود",
+        "Color": "#1B9062",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1775",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "فاسكو دي غاما",
+        "Color": "#313131",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1227",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-21T00:00",
+      "Time-End": "2025-08-21T02:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": 14,
+        "Match-Info": {
+          "Match-Referee": "Matheus Delgado Candançan",
+          "Club-Name": "Estadio Alfredo Jaconi",
+          "Tv": "N/A"
+        }
+      }
+    },
+    {
+      "Cup-id": 102,
+      "Cup-Name": "كوبا ليبيرتادورس - دور الـ 16",
+      "Cup-Logo": "https://www.yanb8.com/api/assets/cup-logo/?id=102",
+      "Match-id": 4443295,
+      "Team-Right": {
+        "Name": "إستوديانتيس",
+        "Color": "#E30F13",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=867",
+        "Goal": 0
+      },
+      "Team-Left": {
+        "Name": "سيرو بورتينيو",
+        "Color": "#CF261F",
+        "Logo": "https://www.yanb8.com/api/assets/team-logo/?id=1294",
+        "Goal": 0
+      },
+      "Time-Start": "2025-08-21T00:00",
+      "Time-End": "2025-08-21T02:00",
+      "Time-Now": -1,
+      "Time-Zone": "+02:00",
+      "Match-Status": "لم تبدأ",
+      "info": {
+        "Round": null,
+        "Match-Info": {
+          "Match-Referee": "Jesús Valenzuela Sáez",
+          "Club-Name": "Jorge Luis Hirschi",
+          "Tv": "N/A"
+        }
+      }
+    }
+  ]
+}
